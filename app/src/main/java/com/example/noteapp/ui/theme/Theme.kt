ff0eac728a076e8a5122cde64cb4ccac35387069
Passant-Hatem <79157384+Passant-Hatem@users.noreply.github.com>
2022-10-12 23:55:31 +0200

    fix: theme

--- app/src/main/java/com/example/noteapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/noteapp/ui/theme/Theme.kt
@@ -1,9 +1,7 @@
 package com.example.noteapp.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
@@ -15,32 +13,8 @@ private val DarkColorPalette = darkColors(
     onSurface = DarkGray
 )
 
-//TODO light theme
-//private val LightColorPalette = lightColors(
-//    primary = Purple500,
-//    primaryVariant = Purple700,
-//    secondary = Teal200
-//
-//    /* Other default colors to override
-//    background = Color.White,
-//    surface = Color.White,
-//    onPrimary = Color.White,
-//    onSecondary = Color.Black,
-//    onBackground = Color.Black,
-//    onSurface = Color.Black,
-//    */
-//)
-
-//darkTheme: Boolean = isSystemInDarkTheme()
 @Composable
-fun NoteAppTheme(darkTheme: Boolean = true, content: @Composable () -> Unit) {
-
-//    val colors = if (darkTheme) {
-//        DarkColorPalette
-//    } else {
-//        LightColorPalette
-//    }
-
+fun NoteAppTheme(content: @Composable () -> Unit) {
     MaterialTheme(
         colors = DarkColorPalette,
         typography = Typography,
